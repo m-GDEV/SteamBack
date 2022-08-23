1af2290a4c25428497d9ee3c9e052f97f0cc2437
Musa Ahmed <musaa.ahmed7@gmail.com>
2022-08-23 17:32:22 -0400

    api key

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 export default function Dashboard() {
-    const key = "717EDBD2C011A91505EE606EF6ACF717";
+    const key = "66FE24C9CDA273164A5A57D6B610185D";
     const steamID = "76561198385824684";
     const [data, setData] = useState([]);
     const url = `https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${key}&steamid=${steamID}&format=json`;
